54dc5a8bdcc6029de519b0fac2f7a205e78f6b40
songran <821687825@qq.com>
2019-07-19 17:25:46 +0800

    up

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 小程序相关接口
 
 1.生成小程序二维码（可以带参数，修改图片大小）
+  composer require songran/wx dev-master
 
--- test.php
+++ test.php
@@ -1,11 +1,12 @@
 <?php 
-include './src/Qrcode.php';	 
-	//use wx\qrcode ;
-	$config =[
-		 'app_id'  => 'xx',// 评测小程序 微信appid
-	 	  'secret'  => 'xx', //评测小程序 
+	require_once __DIR__ . '/vendor/autoload.php';  
+	use Wx\Qrcode;  
+ 
+ 	$config =[
+		'app_id'  => 'wx139f794d8d1cd210',// 评测小程序 微信appid
+	 	  'secret'  => '38d129acd65c08b4a505b1c4f762704e', //评测小程序 
 	];
-	$mod = new Wx\Qrcode($config);
+	$mod = new Qrcode($config);
 	$base64_image =  $mod->getBase64Img('pages/index/index?qrcode=helloworld');
 	//echo $base64_image;exit;
 	//exit;
